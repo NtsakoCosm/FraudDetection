1be01414ac55dbf51703e1908ea10a25025b597d
NtsakoCosm <75445375+NtsakoCosm@users.noreply.github.com>
2022-05-27 00:26:20 +0200

    Update EDA.py

--- Fraud/Production/EDA.py
+++ Fraud/Production/EDA.py
@@ -1,12 +1,6 @@
-from matplotlib import projections
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import streamlit as st
 from PIL import Image
-import plotly.express as px
+
 
 
 def Eda():
